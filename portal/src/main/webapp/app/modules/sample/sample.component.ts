3c7f6ec2975cabf9eb1805926d2e753636728482
SRIDHAR DASARI <REDDY@Reddy.local>
2019-04-24 01:29:54 +0530

    added redirection on logout

--- portal/src/main/webapp/app/modules/sample/sample.component.ts
+++ portal/src/main/webapp/app/modules/sample/sample.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { AccountService } from '../../core/auth/account.service';
 import { AuthServerProvider } from '../../core/auth/auth-jwt.service';
 
@@ -8,13 +10,17 @@ import { AuthServerProvider } from '../../core/auth/auth-jwt.service';
     styleUrls: ['./sample.component.scss']
 })
 export class SampleComponent {
-    constructor(private accountService: AccountService, private authServerProvider: AuthServerProvider) {}
+    constructor(private accountService: AccountService, private authServerProvider: AuthServerProvider, private router: Router) {}
 
     logout() {
         if (this.accountService.isAuthenticated()) {
-            this.authServerProvider.logout().subscribe(() => this.accountService.authenticate(null));
+            this.authServerProvider.logout().subscribe(() => {
+                this.accountService.authenticate(null);
+                this.router.navigate(['sample']);
+            });
         } else {
             this.accountService.authenticate(null);
+            this.router.navigate(['sample']);
         }
     }
 }
